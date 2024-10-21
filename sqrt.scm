0f3f1a8fe869c258176a68f8a188acb192b54527
phedias <daniel.borgs@noxa.de>
2024-10-21 08:35:05 +0200

    fix: use correct syntax

--- sqrt.scm
+++ sqrt.scm
@@ -1,30 +1,32 @@
-(define (root x)
+(define (root_own x)
+	(define (root-iter guess x)
+		(if (good-enough? guess x)
+			guess
+			(root-iter (improve guess x) x)
+		)
+	)
+
+	(define (sqr_own x)
+		(* x x)
+	)
+
+	(define (improve guess x)
+		(mean guess (/ x guess))
+	)
+
+	(define (good-enough? guess x)
+		(<
+			(abs (- (sqr_own guess) x))
+			0.00001
+		)
+	)
+
+	(define (mean guess x)
+		(/
+			(+ guess x)
+			2.0
+		)
+	)
+
 	(root-iter 1.0 x)
-)
-
-(define (root-iter guess x)
-	(if (good-enough? guess x)
-		guess
-		(root-iter (improve guess x) x)
-	)
-)
-
-(define (sqr x) (x * x))
-
-(define (good-enough guess x)
-	(<
-		(abs (- (sqr guess) x))
-		0.00001
-	)
-)
-
-(define (improve guess x)
-	(mean guess (/ x guess))
-)
-
-(define (mean guess x)
-	(/
-		(guess + x) 
-		2
-	)
 )
