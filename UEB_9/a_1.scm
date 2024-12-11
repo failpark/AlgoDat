645d21de506714c2eb39d8be47a5f3f7cb7a3da7
phedias <daniel.borgs@noxa.de>
2024-12-11 22:15:03 +0100

    fix: wrap list around pair and use built-in append

--- UEB_9/a_1.scm
+++ UEB_9/a_1.scm
@@ -1,17 +1,10 @@
 #lang racket
 (define (tuerme-von-hanoi n)
-	; manually traverse the list and app(end) the element
-	(define (app list_ item)
-		(if (null? list_)
-			(cons item (list))
-			(cons (car list_) (app (cdr list_) item))
-		)
-	)
 	; jesus f-ing christ ...
 	; anyhow ... I think ur supposed to realize / learn how to pass quoted values around ...
 	(define (helper n src tmp dest out)
 		(if (= n 1)
-			(app out (cons src dest))
+			(append out (list (cons src dest)))
 			; doing let* to do one after the other
 			; instead of writing
 			; (helper (- n 1) tmp src dest (append (helper (- n 1) src dest tmp out) (cons src dest))
@@ -23,14 +16,12 @@
 			(let*
 				(
 					(out (helper (- n 1) src dest tmp out))
-					(out (app out (cons src dest)))
+					(out (append out (list (cons src dest))))
 				)
 				(helper (- n 1) tmp src dest out)
 			)
 		)
 	)
-	; reversing the list with reverse did not work because "PROPER LIST" expected
-	; ... whatever the hell that means
 	(helper n 'a 'b 'c (list))
 )
 (tuerme-von-hanoi 3)
