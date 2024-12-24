ad41b2be0aac864f5686386296081eef379b3c6b
phedias <daniel.borgs@noxa.de>
2024-12-24 11:37:25 +0100

    fix: a_3

--- UEB_10/a_3.scm
+++ UEB_10/a_3.scm
@@ -1,11 +1,13 @@
 #lang racket
-(require sdraw)
-(require compatibility/mlist)
-; sadly we HAVE to use racket here since they moved set-car and set-cdr to a different module
-; that thws imports for us
+; sadly we HAVE to use racket here since they moved set-car and set-cdr
+; to m<variant>
+
 
 ; I want to do a struct which is a list of pairs
 ; '((<char> . <num>))
+; the purely functional approach would probably be to cons a new pair with the updated val
+; and to remove the pointers before and after to from the old pair to the new pair
+; which is total overkill for updating a counter .-.
 ; after some searching I found out the name for my structure is a table
 ; https://web.mit.edu/6.001/6.037/sicp.pdf#section*.226
 (define (isAnagramm in1 in2)
@@ -63,21 +65,42 @@
 			table
 		)
 	)
-	(define (check_table table iter)
-		(if (null? iter) #t
-			(if
-				; the lambda is just there as a func to return false
-				(update (char-downcase (car iter)) table sub (lambda (a b) #f))
-				(check_table table (cdr iter))
+	(define (check_pair a b)
+		; and short-circuits and doesn't check the rest if b is false
+		(and b (equal? (mcar a) (mcar b)) (equal? (mcdr a) (mcdr b)))
+	)
+	(define (is_space val)
+		(equal? val #\space)
+	)
+	(define (check a haystack)
+		(if (null? a) #t
+		(let*
+			(
+				(val_a (mcar a))
+				(ptr_b (massoc (mcar val_a) (mcdr haystack)))
+				(a (mcdr a))
+			)
+			; or short-circuits if is_space is true
+			(if (or (is_space val_a) (check_pair val_a ptr_b))
+				(check a haystack)
 				#f
 			)
+		))
+	)
+	(define (check_tables a b)
+		; better would be to remove matching pairs from b and then in the end check if b is empty
+		; but since I don't want to impl more and compute is free we check twice :)
+		(and
+			(check (mcdr a) b)
+			(check (mcdr b) a)
 		)
 	)
-	(let
-		(
-			(table (init_table in1))
-		)
-		(check_table table (string->list in2))
+	(check_tables
+		(init_table in1)
+		(init_table in2)
 	)
 )
 (isAnagramm "Desperation" "A rope ends it")
+(isAnagramm "Eleven plus two" "Twelve plus one")
+(isAnagramm "test" "testing")
+(isAnagramm "testing     " "test")
