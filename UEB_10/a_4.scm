604f20ed443063f7d92e9b3a5a81d381d508e86d
phedias <daniel.borgs@noxa.de>
2024-12-31 12:55:35 +0100

    fix: typo

--- UEB_10/a_4.scm
+++ UEB_10/a_4.scm
@@ -1,4 +1,4 @@
-(define (vector-add . vecs)
+(define (vektor-add . vecs)
 	(define (helper in out)
 		(if (null? in) out
 			(helper (cdr in) (map + out (car in)))
@@ -6,5 +6,5 @@
 	)
 	(helper (cdr vecs) (car vecs))
 )
-(vector-add '(1 2) '(1 2))
-(vector-add '(1 2 3) '(4 5 6) '(7 8 9))
+(vektor-add '(1 2) '(1 2))
+(vektor-add '(1 2 3) '(4 5 6) '(7 8 9))
